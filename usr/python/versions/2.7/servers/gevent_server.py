00a0474f3d5b1fe817282dc08362a6b00b7dee60
Goran Sterjov <goran.sterjov@gmail.com>
2014-02-13 21:12:01 +1100

    Fixed gevent server and trying to install gevent on post-install

--- usr/python/versions/2.7/servers/gevent_server.py
+++ usr/python/versions/2.7/servers/gevent_server.py
@@ -5,7 +5,7 @@ from gevent.pywsgi import WSGIServer
 import app
 
 
-sock_path = "unix:{0}run/appserver.sock".format(os.environ["OPENSHIFT_ADVANCED_PYTHON_DIR"])
+sock_path = "{0}run/appserver.sock".format(os.environ["OPENSHIFT_ADVANCED_PYTHON_DIR"])
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 sock.bind(sock_path)
 sock.listen(256)
