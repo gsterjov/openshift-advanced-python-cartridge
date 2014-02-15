f7b512df5116adbbde0657ffd18969cda46fa9f7
Goran Sterjov <goran.sterjov@gmail.com>
2014-02-15 15:56:19 +1100

    Fixed wsgiref appserver. Cartridge should work out of the box now.

--- usr/python/versions/2.7/servers/wsgiref_server.py
+++ usr/python/versions/2.7/servers/wsgiref_server.py
@@ -4,7 +4,7 @@ from wsgiref.simple_server import make_server
 import app
 
 
-server = make_server('', 15000, app.application)
+server = make_server(os.environ["OPENSHIFT_ADVANCED_PYTHON_IP"], 15000, app.application)
 print "Serving HTTP on port 15000..."
 
 server.serve_forever()
